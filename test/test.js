8b128277fa8a2b0a477d34dd1a942e57d3bd5939
Patipol Wuthiprasert <abcteapatipol@gmail.com>
2019-05-27 13:32:03 +0700

    first test fail

--- test/test.js
+++ test/test.js
@@ -3,6 +3,8 @@ const assert = require('assert');
 
 describe('indexFunc', function() {
   describe('fizzbuxzzfunc', function() {
-    
+    it('should return Fizz if  3', function() {
+      assert.equal(indexFunc.fizzbuzzfunc(3), 'Fizz');
+    });
   });
 });
