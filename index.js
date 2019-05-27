d6e75fec3897130f7ba9fed921ea120e36394702
Patipol Wuthiprasert <abcteapatipol@gmail.com>
2019-05-27 13:28:19 +0700

    delete to rewrite as tdd steps

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const indexFunc = {};
 
 indexFunc.fizzbuzzfunc = function(x) {
-  if (x % 3 == 0 && x % 5 == 0) {
-    return 'FizzBuzz';
-  } else if (x % 3 == 0) {
-    return 'Fizz';
-  } else if (x % 5 == 0) {
-    return 'Buzz';
-  } else {
-    return x;
-  }
+  
 };
 
 module.exports = indexFunc;
--- test/test.js
+++ test/test.js
@@ -3,17 +3,6 @@ const assert = require('assert');
 
 describe('indexFunc', function() {
   describe('fizzbuxzzfunc', function() {
-    it('should return FizzBuzz when %15 == 0', function() {
-      assert.equal(indexFunc.fizzbuzzfunc(15), 'FizzBuzz');
-      assert.equal(indexFunc.fizzbuzzfunc(30), 'FizzBuzz');
-    });
-    it('should return Fizz when %3 == 0', function() {
-      assert.equal(indexFunc.fizzbuzzfunc(3), 'Fizz');
-      assert.equal(indexFunc.fizzbuzzfunc(69), 'Fizz');
-    });
-    it('should return Buzz when %5 == 0', function() {
-      assert.equal(indexFunc.fizzbuzzfunc(5), 'Buzz');
-      assert.equal(indexFunc.fizzbuzzfunc(25), 'Buzz');
-    });
+    
   });
 });
